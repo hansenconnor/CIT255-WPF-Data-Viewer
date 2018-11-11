ca5ae6744cb8d067dd22f03a9bdd51d094aaa813
Connor Hansen <connorhnsn5596@gmail.com>
2018-11-11 11:33:51 -0500

    Detail view working

--- WPF_DataViewer/DetailWindow.xaml.cs
+++ WPF_DataViewer/DetailWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Navigation;
 
 namespace WPF_DataViewer
 {
@@ -40,13 +42,30 @@ namespace WPF_DataViewer
             // Add the title
             _publicationTitle.Content = _selectedPublication.name;
             // Add the URL
-            _publicationLink.Content = _selectedPublication.url;
+            _publicationLink.Inlines.Add(new Run(_selectedPublication.url));
+            Uri publicationLink = new Uri(_selectedPublication.url);
+            _publicationLink.NavigateUri = publicationLink;
+
+            
+
+
+
+            var publicationImage = new Image();
+            var fullFilePath = _selectedPublication.imageUrl;
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(fullFilePath, UriKind.Absolute);
+            bitmap.EndInit();
+
+            _publicationImage.Source = bitmap;
 
         }
 
         private void Label_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
-
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
